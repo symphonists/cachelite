6da6aa7bbace443482ef7ab6b6f71e4a8be678be
max <max@icelab.com.au>
2009-06-24 08:39:55 +1000

    Updated build date and version in driver.

--- extension.driver.php
+++ extension.driver.php
@@ -12,8 +12,8 @@
 		public function about()
 		{
 			return array('name' => 'CacheLite',
-						 'version' => '0.1.2',
-						 'release-date' => '2009-04-20',
+						 'version' => '0.1.3',
+						 'release-date' => '2009-06-22',
 						 'author' => array('name' => 'Max Wheeler',
 											 'website' => 'http://makenosound.com/',
 											 'email' => 'max@makenosound.com'),
