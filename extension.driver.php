symphonists/cachelite

commit 1db4e33fa1fd6c33795b53a492bf490149d38bdc

Actually copy the $_GET array

extension.driver.php
@@ -567,8 +567,7 @@
 		private function updateFromGetValues()
 		{
 			// Cache sorted $_GET;
-			$this->_get = $_GET;
-			ksort($this->_get);
+			$this->_get = ksort(array_merge(array(), $_GET));
 			// hash it to make sure it wont overflow 255 chars
 			$this->_url = $this->computeHash($this->_get);
 		}

commit 4554ee4b6873d4e05605d0f6b8dceb9382637f00

ksort does not return an array
Fixed bug introduced in 1db4e33

extension.driver.php
@@ -630,7 +630,8 @@
 		private function updateFromGetValues()
 		{
 			// Cache sorted $_GET;
-			$this->_get = ksort(array_merge(array(), $_GET));
+			$this->_get = array_merge(array(), $_GET);
+			ksort($this->_get);
 			// hash it to make sure it wont overflow 255 chars
 			$this->_url = $this->computeHash($this->_get);
 		}